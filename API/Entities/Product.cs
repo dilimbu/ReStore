fc6fab0717f3a48d8708805c8f43741e94724ca4
Dipen Limbu <dilimbu@gmail.com>
2024-05-28 17:58:21 -0700

    Update 2 DL

---
+++ API/Entities/Product.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Price { get; set; }
+        public string Type { get; set; }
+        public int InStock { get; set; }
+    }
+}
